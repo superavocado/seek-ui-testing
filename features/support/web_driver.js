d1efc07abb1ede2343e1e8e4cf0ea5ef8134a694
test007 <mywebtest007@gmail.com>
2019-07-17 00:05:24 +1000

    Update web_driver.js
    run test on headless mode

--- features/support/web_driver.js
+++ features/support/web_driver.js
@@ -2,13 +2,18 @@ require('chromedriver');
 //require('geckodriver');
 //require('iedriver'); //add this package if you want to use it.
 const webDriver = require('selenium-webdriver');
+const { Options } = require('selenium-webdriver/chrome');
+let chromeOptions = new Options();
 
 //create WebDriver instance based on your browser config;
 function createDriver() {
     let browserConfig = process.env.BROWSER || 'chrome';
     let browser = browserConfig.toLowerCase();
     if (['chrome', 'firefox', 'ie'].indexOf(browser) < 0) browser = 'chrome'; //default to chrome
-    return new webDriver.Builder().forBrowser(browser).build();
+    return new webDriver.Builder()
+    .forBrowser(browser)
+    .setChromeOptions(chromeOptions.headless())
+    .build();
 }
 
 exports.driver = createDriver();
